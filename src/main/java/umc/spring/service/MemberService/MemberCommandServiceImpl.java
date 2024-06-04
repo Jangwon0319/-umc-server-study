badfe627598dc11fea517961d1ee67f83889b191
Jangwon0319 <ryujw0319@gmail.com>
2024-06-04 18:32:34 +0900

    8주차 미션 근데 9주차를 곁들인

---
+++ src/main/java/umc/spring/service/MemberService/MemberCommandServiceImpl.java
@@ -0,0 +1,13 @@
+package umc.spring.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberCommandServiceImpl{
+
+
+}
